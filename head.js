8215f21826777ce07cc60bdad9a73227e6cecc38
NKadish <noahkadish@hotmail.com>
2020-09-14 19:08:46 +0000

    head.js, takes first item in an array

---
+++ head.js
@@ -0,0 +1,17 @@
+//ASSERT EQUAL FUNCTON, TESTS IF THEY ARE EQUAL
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//HEAD FUNCTION, TAKES OUT THE FIRST ITEM OF AN ARRAY
+const head = function(array) {
+  return array[0];
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
