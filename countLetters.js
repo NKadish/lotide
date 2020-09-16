10110e5957dc021cf53096f102d4f7039fac23e2
NKadish <noahkadish@hotmail.com>
2020-09-16 17:59:41 +0000

    A tool to count the number of times each letter appears in a sentence

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const results = {};
+  for (const letters of sentence) {
+    if (letters === ' ') { //if it's a space don't count it 
+      //not satisfied with this, will ask someone about a cleaner way to not include spaces later 
+    } else if (results[letters]) { //if the letter is already in results, add one
+      results[letters]++;
+    } else { //if there is no letter in results, make it and initialize it to one. 
+      results[letters] = 1;
+    }
+  }
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"));
