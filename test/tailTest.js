b1d1a84d3f77d9c4d04953ff553ceeb2f956c6b0
NKadish <noahkadish@hotmail.com>
2020-09-21 18:38:21 +0000

    refactored tail and middle test files.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); //Yea I get it, so basically this thing is applying tails to words, but since words doesn't change, it only creates a new array, when you check words later it is not the new array but the words array.
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
 
-module.exports = assertEqual;
-module.exports = tail;
+});
+
