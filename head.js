58668b713e8e69e1895ca5c56794f683474963f9
NKadish <noahkadish@hotmail.com>
2020-09-21 17:21:48 +0000

    Some refactoring, exporting module to use in other files and made a test folder to hold the tests.

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-//ASSERT EQUAL FUNCTON, TESTS IF THEY ARE EQUAL
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 //HEAD FUNCTION, TAKES OUT THE FIRST ITEM OF AN ARRAY
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
