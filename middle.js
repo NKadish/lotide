45624e865b268bc663cdd52e3c5df1a2052933f2
NKadish <noahkadish@hotmail.com>
2020-09-21 18:05:09 +0000

    Refactored middle and added middleTest.js

--- middle.js
+++ middle.js
@@ -1,29 +1,5 @@
-const eqArrays = function(array1, array2) {
-  let truthChecker = false; //baseline for whether the arrays are the same
-  if (array1.length !== array2.length) {
-    truthChecker = false;
-    return truthChecker;
-  } else {
-    for (let x = 0; x < array1.length; x++) { //loop through the arryas
-      if (array1[x] === array2[x]) { //if the two arrays are strictly equal
-        truthChecker = true; //turns the baseline to true
-      } else if (array1[x] !== array2[x]) {
-        truthChecker = false;
-        return truthChecker; //if they're not strictly equal, it makes the checker false and exits with return
-      }
-    }
-    return truthChecker;
-  }
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  let output = eqArrays(arrayOne, arrayTwo);
-  if (output === true) {
-    console.log("Hey sick dude the two are the same!");
-  } else {
-    console.log("Sorry my guy the two arrays are not the same.");
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const middle = function (array) {
   let middleHolder = []; //accumulator, where I'll put the middle. 
@@ -42,4 +18,5 @@ const middle = function (array) {
     return middleHolder;
   }
 }
-console.log(middle([1, 2]));
+
+module.exports = middle;
